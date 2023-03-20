b302b316cd1e8069b7d6e5cf184fba19376d253e
rustygate229 <karnati.10@osu.edu>
2023-03-20 10:07:09 -0400

    Checkpoint 2 completed

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <FEHUtility.h>
 
 #define RADIUS 1.75
-#define LEFT_MOTORSPEED -27.0
-#define RIGHT_MOTORSPEED 25.0
-#define RAMP_LEFT -37.0
+#define LEFT_MOTORSPEED -28.0
+#define RIGHT_MOTORSPEED 26.0
+#define RAMP_LEFT -35.0
 #define RAMP_RIGHT 35.0
 #define PI 3.1415927
 
@@ -20,6 +20,7 @@ void move(float a, float distance);
 void ramp(int a, float distance);
 void rotate(int direction, float angle);
 bool detectLight(AnalogInputPin a);
+int kioskLight(AnalogInputPin);
 
 int main(void)
 {
@@ -27,30 +28,73 @@ int main(void)
 
     while (!detectLight(cdsCell));
 
-    // hits kiosk
-    rotate(1, 60.0);
-    move(-1, 5);
-    rotate(-1, 8);
-    move(-1, 9);
-    ramp(-1, 18);
-    move(-1, 2);
-    rotate(-1, 45.0);
-    move(-1, 13);
-    rotate(1, 45.0);
-    move(-1, 14);
+    // gets to plane
+    
+    rotate(1, 65.0); // initial rotation
+    Sleep(0.5);
+    move(-1, 10); // move closer to ramp
+    Sleep(0.5);
+    rotate(-1, 17.0); // correction rotation
+    Sleep(0.5);
+    ramp(-1, 22); // goes up the ramp
+    Sleep(0.5);
 
-    // comes back down the ramp
-    move(1, 12);
-    rotate(-1, 45.0);
-    move(1, 13);
-    rotate(1, 50.0);
-    move(1, 22);
+    
+    move(-1, 2); // moves 2 inches from the ramp
+    Sleep(0.5);
+    rotate(1, 90); // rotates clockwise 90 degrees
+    Sleep(0.5);
+    move(-1, 7); // lines back up with wall
+    Sleep(0.5);
+    move(1, 22); // moves forward 22 inches
+
+    // robot at middle of airplane
+
+    //gets to light
+    rotate(1, 80); // rotates to light
+    
+    // moves 1 inch at a time until it detects light
+    do {
+        move(1, 1);
+    } while (!detectLight(cdsCell));
+
+    float time = TimeNow();
+    int color = 0;
+
+    // detects light for 2 seconds
+    while (TimeNow() - time < 2) {
+        color = kioskLight(cdsCell);
+    }
+
+    // moves back for clearance
+    move(-1, 3);
+    rotate(-1, 85); // rotates to travel to button
+    
+    // if the light is blue
+    if (color == 1) {
+        move(-1, 5); 
+    // if light is red
+    } else if (color == 2) {
+        move(-1, 10.5);
+    }
+
+    // rotate and crash into button
+    rotate(-1, 78);
+    move(-1, 10);
+
+    // move back down the ramp
+    move(1, 14);
+    rotate(1, 80);
+    move(-1, 18);
+    move(1, 3);
+    rotate(1, 80);
+    move(-1, 15);
 }
 
 bool detectLight(AnalogInputPin cds) {
 
     // checks if light is on
-    if (cds.Value() < 1.5) {
+    if (cds.Value() < 1) {
         return true;
     }
 
@@ -119,10 +163,41 @@ void rotate(int direction, float angle) {
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+    float time = TimeNow();
+
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts) {
+        if (TimeNow() - time > 6) {
+            break;
+        }
+    }
     
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
 
+}
+
+int kioskLight(AnalogInputPin cds) {
+
+    LCD.Clear();
+    float cdsValue = cds.Value();
+
+    // if cds cell detects light
+    if (cdsValue < 1.5) {
+        // if light is blue
+        if (cdsValue > 0.4) {
+            LCD.SetBackgroundColor(BLUE);
+            return 1;
+        // if light is red
+        } else {
+            LCD.SetBackgroundColor(RED);
+            return 2;
+        }
+    
+    // cell does not detect light
+    } else {
+        LCD.SetBackgroundColor(BLACK);
+        LCD.WriteLine("Do not detect light");
+        return 0;
+    }
 }
